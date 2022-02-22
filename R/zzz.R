934b640df292a4160451e3c913bc7437a8dd7f4b
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2022-02-22 14:05:17 -0500

    add spaces in startup

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   version <- packageDescription(pkgname, fields = "Version")
   msg <- paste0("============
-", pkgname, "version", version, "
+", "This is ", pkgname, " version ", version, "
 GitHub page: https://github.com/jackbibby1/SCPA
 Package tutorials: https://jackbibby1.github.io/SCPA/
 If you use this, please cite: https://www.biorxiv.org/content/10.1101/2022.02.07.478807v1

fc6e4d8dc5f134341ddf3d5da7b735748397383d
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2022-02-22 14:15:25 -0500

    change startup

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
   version <- packageDescription(pkgname, fields = "Version")
   msg <- paste0("============
 ", "This is ", "version ", version, " of ", pkgname, "
-GitHub page: https://github.com/jackbibby1/SCPA
-Package tutorials: https://jackbibby1.github.io/SCPA/
+SCPA GitHub page: https://github.com/jackbibby1/SCPA
+For SCPA tutorials: https://jackbibby1.github.io/SCPA/
 If you use this, please cite: https://www.biorxiv.org/content/10.1101/2022.02.07.478807v1
 ============
 ")

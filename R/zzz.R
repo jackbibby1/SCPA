5b956d0feef5485c6e03e82f38d99f5ec81b1d50
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2022-02-22 14:16:55 -0500

    add extra spacing

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onAttach <- function(libname, pkgname) {
   version <- packageDescription(pkgname, fields = "Version")
-  msg <- paste0("============
+  msg <- paste0("====================
 ", "This is ", "version ", version, " of ", pkgname, "
 SCPA GitHub page: https://github.com/jackbibby1/SCPA
 For SCPA tutorials: https://jackbibby1.github.io/SCPA/
 If you use this, please cite: https://www.biorxiv.org/content/10.1101/2022.02.07.478807v1
-============
+====================
 ")
 
   packageStartupMessage(msg)

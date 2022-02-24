38d445a68bca5f94a18bc872cf2219aa94c219f4
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2022-02-24 10:56:36 -0500

    change citation message

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,6 @@
 ", "This is ", "version ", version, " of ", pkgname, "
 SCPA GitHub page: https://github.com/jackbibby1/SCPA
 For SCPA tutorials: https://jackbibby1.github.io/SCPA/
-If you use this, please cite: https://www.biorxiv.org/content/10.1101/2022.02.07.478807v1
 ====================
 ")
 

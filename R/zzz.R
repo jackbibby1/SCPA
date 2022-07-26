0a5ccd7bda48838f2dcf928b3346c1ad6b407f6d
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2022-07-26 15:25:11 -0400

    add info to startup

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   version <- utils::packageDescription(pkgname, fields = "Version")
   msg <- paste0("====================
 ", "This is ", "version ", version, " of ", pkgname, "
-SCPA GitHub page: https://github.com/jackbibby1/SCPA
+For the SCPA GitHub page: https://github.com/jackbibby1/SCPA
 For SCPA tutorials: https://jackbibby1.github.io/SCPA/
 ====================
 ")
